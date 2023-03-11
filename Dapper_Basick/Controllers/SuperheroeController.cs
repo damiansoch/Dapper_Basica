fe88098a6aaa4c8b428b80485f492053c9fdb4ce
Damian Socha <damiansoch@hotmail.com>
2023-03-11 20:11:18 +0000

    Add project files.

---
+++ Dapper_Basick/Controllers/SuperheroeController.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Dapper_Basick.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Dapper_Basick.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperheroeController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public SuperheroeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllSuperheroes()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var heroes = await connection.QueryAsync<SuperHero>("SELECT * FROM Supercheroes");
+            return Ok(heroes);
+        }
+    }
+}
---
+++ Dapper_Basick/Models/SuperHero.cs
@@ -0,0 +1,10 @@
+﻿namespace Dapper_Basick.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Hero { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
